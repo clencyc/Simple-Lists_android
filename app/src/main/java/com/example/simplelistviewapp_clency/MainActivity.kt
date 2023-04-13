98daa7dfba75aece6d2ac727d1197c338b2ed598
emobilis <clency2023@gmail.com>
2023-04-13 10:28:52 +0300

    initial commit

---
+++ app/src/main/java/com/example/simplelistviewapp_clency/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.simplelistviewapp_clency
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+}
